9cb8c98d93d6d4126944e874e275b02f698418c6
YaRo <roman.yatsenko@hneu.net>
2025-12-03 19:28:20 +0200

    Refactor product list template to improve layout handling for additional product cards

--- shop/views.py
+++ shop/views.py
@@ -10,11 +10,14 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    additional_cards = 3 - products.count() % 3
+    print(additional_cards)
     return render(request,
                   'shop/product/list.html',
                   {'category': category,
                    'categories': categories,
-                   'products': products})
+                   'products': products,
+                   'additional_cards': additional_cards})
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
