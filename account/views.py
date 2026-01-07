738102d65296af0ac764aead5031cbe4ec86c5c6
YaRo <roman.yatsenko@hneu.net>
2026-01-07 21:04:20 +0200

    Add profile update success and error messages; implement message display in base template

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -18,6 +19,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Профіль було успішно змінено')
+        else:
+            messages.error(request, 'Помилка оновлення профілю')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
