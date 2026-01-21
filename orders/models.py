a48501b1410cd0fee659ce424608a2e100c499e4
YaRo <roman.yatsenko@hneu.net>
2026-01-21 19:37:02 +0200

    Add Order and OrderItem models with verbose names; update admin registration for Order

--- orders/models.py
+++ orders/models.py
@@ -31,11 +31,14 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="items", 
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE,
+                              verbose_name='Замовлення')
     product = models.ForeignKey(Product, related_name="order_items", 
-                                on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+                                on_delete=models.CASCADE,
+                                verbose_name="Товар")
+    price = models.DecimalField(max_digits=10, decimal_places=2, 
+                                verbose_name="Ціна")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Кількість")
 
     def __str__(self):
         return str(self.id)
